b1e7230e115dea275123cfdb79e95fc4b8617500
BenjaminClaro <benjamin.claro@free.fr>
2024-09-19 16:36:16 +0200

    modified:   monProjet/config/routes.yaml 	modified:   monProjet/src/Controller/IndexController.php 	modified:   monProjet/src/Controller/SouscategoriesController.php 	modified:   monProjet/templates/index/index.html.twig 	modified:   monProjet/templates/souscategories/index.html.twig 	new file:   monProjet/templates/souscategories/souscat_id.html.twig

--- monProjet/src/Controller/IndexController.php
+++ monProjet/src/Controller/IndexController.php
@@ -7,18 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\RubriquesRepository;
+use App\Repository\SousRubriquesRepository;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(RubriquesRepository $rubriquesRepository): Response
+    public function index(RubriquesRepository $rubriquesRepository, SousRubriquesRepository $sousrubriquesRepository): Response
     {
 
         $rubriques = $rubriquesRepository->findAll();
+        $sousrubriques = $sousrubriquesRepository->findAll();
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'rubriques' => $rubriques,
+            'sousrubriques' => $sousrubriques,
         ]);
     }
 }
--- monProjet/src/Controller/SouscategoriesController.php
+++ monProjet/src/Controller/SouscategoriesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Templates\Index\index;
 use App\Repository\SousRubriquesRepository;
 use App\Repository\RubriquesRepository;
 
 
 class SouscategoriesController extends AbstractController
-{
-    #[Route('/souscategories', name: 'app_souscategories')]
+    {#[Route('/souscategories', name: 'app_souscategories')]
     public function index(RubriquesRepository $rubriquesRepository, SousRubriquesRepository $sousrubriquesRepository): Response
     {
 
@@ -23,6 +23,20 @@ class SouscategoriesController extends AbstractController
             'controller_name' => 'SouscategoriesController',
             'sousrubriques' => $sousrubriques,
             'rubriques' => $rubriques,
+        ]);
+    }
+
+    #[Route('/souscategories/{rubrique}', name: 'app_souscategories2', methods:['GET'])]
+    public function index2(RubriquesRepository $rubriquesRepository, SousRubriquesRepository $sousrubriquesRepository, int $rubrique): Response
+    {
+
+
+        $rubriques = $rubriquesRepository->find($rubrique);
+
+        return $this->render('souscategories/souscat_id.html.twig', [
+            'controller_name' => 'SouscategoriesController',
+
+            'sousrubriques' => $sousrubriquesRepository->findBy(['rubriques'=>$rubrique]),
         ]);
     }
 }
